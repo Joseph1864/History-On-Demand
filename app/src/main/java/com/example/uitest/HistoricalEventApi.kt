aa3d06ac23cd5c5f3cc186c17566d3eb460cd23a
Joe <josephwilliamsm99@gmail.com>
2023-03-28 16:45:38 -0700

    added okhttp

--- app/src/main/java/com/example/uitest/HistoricalEventApi.kt
+++ app/src/main/java/com/example/uitest/HistoricalEventApi.kt
@@ -1,12 +1,20 @@
 package com.example.uitest
 
+import okhttp3.OkHttp
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class HistoricalEventApi {
+    private val httpClient = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY})
+        .build()
+
     private val retrofit = Retrofit.Builder()
         .baseUrl("https://api.api-ninjas.com/v1/historicalevents/")
         .addConverterFactory(GsonConverterFactory.create())
+        .client(httpClient)
         .build()
 
     private val service = retrofit.create(HistoricalEventApiService::class.java)
